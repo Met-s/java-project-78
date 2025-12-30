dfbaac3f2849b5cee9e82b389a2b2ad5244cfd6b
Met-s <feronts@mail.ru>
2025-12-30 17:12:19 +0300

    In Sonar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("idea")
-    id("org.sonarqube") version "7.0.1.6134"
+    id ("org.sonarqube") version "7.2.2.6593"
     id("com.github.ben-manes.versions") version "0.52.0"
     application
     checkstyle
