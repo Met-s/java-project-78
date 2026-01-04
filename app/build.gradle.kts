924cbde24a03f73a91b6aec5a044eea9b834c0ca
Met-s <feronts@mail.ru>
2026-01-04 19:16:18 +0300

    Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("java")
+    id("idea")
+    id ("org.sonarqube") version "7.2.2.6593"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+jacoco {
+    toolVersion = "0.8.13"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+sonar {
+    properties {
+        property ("sonar.projectKey", "Met-s_java-project-78")
+        property ("sonar.organization", "met-s")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.junit.platform:junit-platform-console:1.13.1")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
+
+    implementation ("commons-io:commons-io:2.14.0")
+
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    implementation ("com.fasterxml.jackson.core:jackson-annotations:2.12.4")
+    implementation ("com.fasterxml.jackson.core:jackson-core:2.15.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.1")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events ("passed", "skipped", "failed")
+    }
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
